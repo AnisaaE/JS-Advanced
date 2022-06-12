94e0d3d6f20a9acab4e38ddd1a10f9ac593abb8c
AnisaaE <89021659+AnisaaE@users.noreply.github.com>
2022-06-12 19:39:46 +0300

    Add files via upload

---
+++ Advanced Function/stokeManeger.js
@@ -0,0 +1,10 @@
+function robot(...order){
+
+}robot()
+
+result = result();
+
+let expectationPairs = [
+    ['restock flavour 50', 'Success'],
+    ['prepare lemonade 4', 'Error: not enough carbohydrate in stock']
+];
